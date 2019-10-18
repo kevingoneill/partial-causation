36d7abc55afc1652b0263cccef479c17359b7d7a
kevingoneill <oneillkeving@gmail.com>
2019-10-18 10:46:27 -0400

    added vignette-wise histograms and random effects

--- analysis.r
+++ analysis.r
@@ -1,3 +1,4 @@
+#!/usr/bin/Rscript
 library(brms)
 library(ggplot2)
 
@@ -12,6 +13,7 @@ judgments <- read.csv(args[1], header=TRUE)
 judgments$rating <- judgments$rating / 100
 judgments$confidence <- judgments$confidence / 100
 judgments$condition <- as.factor(judgments$condition)
+judgments$n <- factor(judgments$n)
 
 time <- aggregate(duration ~ id, judgments, function(x) mean(x) / 60.0)
 mean(time$duration)
@@ -19,39 +21,58 @@ sd(time$duration) / sqrt(nrow(time))
 
 
 
-mNormal <- brm(bf(rating ~ n * condition + (1 |v| vignette),
-                  sigma ~ n * condition +  (1 |v| vignette)),
+## binned_pp_check(model, judgments)
+##
+##  Compute/display pp_checks for a model for different levels of confidence.
+##  Uses type='hist' to avoid problems with bandwidth
+##
+binned_pp_check <- function(model, judgments) {
+    for (c in levels(judgments$condition)) {
+        for (n in unique(judgments$n)) {
+            j <- judgments[judgments$n == n & judgments$condition == c, ]
+            
+            if (!is.null(nrow(j)) && nrow(j) > 1) {
+                print(pp_check(model, bw=0.01, newdata=j) + theme_bw() +
+                      ggtitle(sprintf("Ratings (%s cause(s), %s)", n, c)))
+            }
+        }
+    }
+}
+
+
+mNormal <- brm(bf(rating ~ n * condition + (1 + n*condition |v| vignette),
+                  sigma ~ n * condition +  (1 + n*condition |v| vignette)),
                prior=c(set_prior('normal(0, 10.0)', class='b'),
                        set_prior('normal(0, 10.0)', class='b', dpar='sigma')),
-               data=judgments, file='mNormal', inits="0",
-               warmup=1500, iter=3000,
+               data=judgments, file='mNormal_factor', inits="0",
+               warmup=1500, iter=3000, cores=4,
                control=list(adapt_delta=0.99))
 
 summary(mNormal)
-pdf("Normal.pdf")
+pdf("Normal_factor.pdf")
 plot(mNormal)
-pp_check(mNormal, bw=0.005) + theme_bw()
+pp_check(mNormal, bw=0.01) + theme_bw()
+binned_pp_check(mNormal, judgments)
 marginal_effects(mNormal, resp='rating', probs=c(0.05, 0.95))
-#marginal_effects(mNormal, resp='confidence', probs=c(0.05, 0.95))
 dev.off()
 
 
 
-mZOIB <- brm(bf(rating ~ n * condition + (1 |v| vignette),
-                phi ~ n * condition +  (1 |v| vignette),
-                zoi ~ n * condition +  (1 |v| vignette),
-                coi ~ n * condition +  (1 |v| vignette)),
+mZOIB <- brm(bf(rating ~ n * condition + (1 + n*condition |v| vignette),
+                phi ~ n * condition +  (1 + n*condition |v| vignette),
+                zoi ~ n * condition +  (1 + n*condition |v| vignette),
+                coi ~ n * condition +  (1 + n*condition |v| vignette)),
              prior=c(set_prior('normal(0, 10.0)', class='b'),
                      set_prior('normal(0, 10.0)', class='b', dpar='phi'),
                      set_prior('normal(0, 10.0)', class='b', dpar='zoi'),
                      set_prior('normal(0, 10.0)', class='b', dpar='coi')),
-             data=judgments, family=zero_one_inflated_beta(),
-             file='mZOIB', inits="0", control=list(adapt_delta=0.95))
+             data=judgments, family=zero_one_inflated_beta(), cores=4,
+             file='mZOIB_factor', inits="0", control=list(adapt_delta=0.95))
 
 summary(mZOIB)
-pdf("ZOIB.pdf")
+pdf("ZOIB_factor.pdf")
 plot(mZOIB)
-pp_check(mZOIB, bw=0.005) + theme_bw()
+pp_check(mZOIB, bw=0.01) + theme_bw()
+binned_pp_check(mZOIB, judgments)
 marginal_effects(mZOIB, resp='rating', probs=c(0.05, 0.95))
-#marginal_effects(mZOIB, resp='confidence', probs=c(0.05, 0.95))
 dev.off()
--- histograms.r
+++ histograms.r
@@ -1,3 +1,4 @@
+#!/usr/bin/Rscript
 library(dplyr)
 library(gplots)
 library(RColorBrewer)
@@ -16,18 +17,20 @@ plot_hist <- function(data) {
     rf <- colorRampPalette(rev(brewer.pal(11, 'Spectral')))
     COLORS <- rf(32)
 
-    r <- hist(data$rating, breaks=101, plot=F)$counts
-    c <- hist(data$confidence, breaks=101, plot=F)$counts
-
-    par(mar=c(11,11,1,1), mgp=c(8,2.5,0))
-    layout(matrix(c(2,0,1,3), 2, 2, byrow=T), c(8,2), c(2,8))
-
+    breaks = (0:101 - 0.5) / 100
+    r <- hist(data$rating, breaks=breaks, plot=F)$counts
+    c <- hist(data$confidence, breaks=breaks, plot=F)$counts
+    
+    par(mar=c(12,12,0,0), mgp=c(6,2,0))
+    layout(matrix(c(2,0,1,3), 2, 2, byrow=T), c(3,1), c(1,3))
+    
     hist2d(data$rating, data$confidence, nbins=50, FUN=function(x) log(length(x)),
-           xlab='Rating', ylab='Confidence', col=COLORS, cex.axis=3.0, cex.lab=4.0)
-    par(mar=c(0,8,2,0))
-    barplot(r, axes=F, ylim=c(0, max(r)), space=0, col='red')
-    par(mar=c(8,0,0,2))
-    barplot(c, axes=F, xlim=c(0, max(c)), space=0, col='red', horiz=T)
+           xlim=c(0, 1.0), ylim=c(0, 1.0), xlab='Rating', ylab='Confidence',
+           col=COLORS, cex.axis=3.0, cex.lab=4.0)
+    par(mar=c(0,10,2,0))
+    barplot(r, ylim=c(0, max(r)), xlim=c(0, 100), axes=F, space=0, col='red')
+    par(mar=c(10,0,0,2))
+    barplot(c, xlim=c(0, max(c)), ylim=c(0, 100), axes=F, space=0, col='red', horiz=T)
 }
 
 
@@ -44,15 +47,24 @@ png(paste0(DIR, 'hist_all.png'), height=1000, width=1000)
 plot_hist(judgments)
 dev.off()
 
-nrow(judgments)
-str(judgments)
-
 for (nc in unique(judgments$n)) {
     for (c in levels(judgments$condition)) {
-        print(sprintf('Condition: %d %s', nc, c))
-        print(nrow(subset(judgments, n==nc & condition==c)))
         png(paste0(DIR, sprintf('hist_%d_%s.png', nc, c)), height=1000, width=1000)
         plot_hist(subset(judgments, n==nc & condition==c))
         dev.off()
+    }
+}
+
+for (v in levels(judgments$vignette)) {
+    png(paste0(DIR, v, '/hist_all.png'), height=1000, width=1000)
+    plot_hist(subset(judgments, vignette==v))
+    dev.off()
+        
+    for (nc in unique(judgments$n)) {
+        for (c in levels(judgments$condition)) {
+            png(paste0(DIR, v, sprintf('/hist_%d_%s.png', nc, c)), height=1000, width=1000)
+            plot_hist(subset(judgments, n==nc & condition==c & vignette==v))
+            dev.off()
+        }
     }
 }
